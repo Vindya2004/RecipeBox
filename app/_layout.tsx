b6d90a89b30a43ab3368a30ba081653341dec2b4
Vindya2004 <vindyamadubhashini@gmail.com>
2026-01-22 21:24:41 +0530

    add authLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,16 +2,20 @@ import { View } from "react-native"
 import React from "react"
 import { Slot } from "expo-router"
 import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context"
+// import { LoaderProvider } from "@/context/LoaderContext"
+// import { AuthProvider } from "@/context/AuthContext"
 
 const RootLayout = () => {
   const insets = useSafeAreaInsets()
 
   return (
-   
+    // <LoaderProvider>
+    //   <AuthProvider>
         <View style={{ marginTop: insets.top, flex: 1 }}>
           <Slot />
         </View>
-    
+    //   </AuthProvider>
+    // </LoaderProvider>
   )
 }
 
